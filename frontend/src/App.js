a798943ec788b25ede915f6b1696d41444828b35
mickeybin2211 <94766118+MagicDinosaur@users.noreply.github.com>
2022-05-17 16:24:45 -0400

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './components/Login';
 function App() {
   return (
-    <div className="App">
-        <h1>hello</h1>
-    </div>
+      <div>
+          <Router>
+            <Routes>
+               <Route exact path="/login" element={<Login/>}/>
+
+            </Routes>
+              </Router>
+        </div>
   );
 }
 
