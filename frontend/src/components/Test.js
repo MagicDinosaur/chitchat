8acee7429134e6f24b80af1b2f6d237d04593a7d
mickeybin2211 <94766118+MagicDinosaur@users.noreply.github.com>
2022-05-19 17:56:25 -0400

    login/signup form

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -4,16 +4,11 @@ import {connect} from 'react-redux';
 import {userPostFetch} from 'redux-actions';
 function Test(props) {
   const [num, setNum] = useState(0);
-  function logtest() {
-  console.log("hi");
-  }
-
   return (
   <div>
     <h1>Hello {num}</h1>
     <div >
     <button onClick={() => {setNum(num +1)}}> Check</button>
-      <button onClick={logtest()}> Check</button>
     </div>
   </div>
   );
