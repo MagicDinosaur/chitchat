94dbf5ff07a3a66ef2b6915969a7817f791977a0
mickeybin2211 <94766118+MagicDinosaur@users.noreply.github.com>
2022-05-19 18:04:03 -0400

    login/signup form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,34 +4,26 @@ import React, {Component, useState, useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
 import {userPostFetch} from 'redux-actions';
 function Login(props) {
-  const [time, setTime] = useState((new Date()).toString());
-
-
-  useEffect(() => {
-    const intervalId = setInterval(() =>{setTime((new Date()).toString())}
-    , 1);
-    return () => clearInterval(intervalId);
-  }, []);
-
-
+  const [button, setButton] = useState(true);
   return (
   <div>
-    <div className="container" id="container">
-  {/*    Form dang ky /}
-  {/*<div className="form-container sign-up-container">*/}
-  {/*  <form action="#">*/}
-  {/*    <h1>Create Account</h1>*/}
-  {/*    <span>or use your email for registration</span>*/}
-  {/*    <input type="text" placeholder="Name" />*/}
-  {/*    <input type="email" placeholder="Email" />*/}
-  {/*    <input type="password" placeholder="Password" />*/}
-  {/*    <button>Sign Up</button>*/}
-  {/*  </form>*/}
-  {/*</div>*/}
 
+    <div className={button ? "container" : "container right-panel-active"} id="container">
+   {/*Form dang ky*/}
+  <div className="form-container sign-up-container">
+    <form action="#">
+      <h2>Create Account</h2>
+      <span>or use your email for registration</span>
+      <input type="text" placeholder="Name" />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button >Sign Up</button>
+    </form>
+  </div>
+{/*Form dang nhap*/}
   <div className="form-container sign-in-container">
     <form action="#">
-      <p>Testing time: {time} </p>
+      {/*<p>Testing time: {time} </p>*/}
       <h1>Sign in</h1>
       <span>Enjoy your endless fun</span>
       <input type="email" placeholder="Email" />
@@ -41,18 +33,18 @@ function Login(props) {
     </form>
   </div>
 
-
+{/*form welcome*/}
   <div className="overlay-container">
     <div className="overlay">
       <div className="overlay-panel overlay-left">
         <h1>Welcome Back!</h1>
         <p>To keep connected with us please login with your personal info</p>
-        <button className="ghost" id="signIn">Sign In</button>
+        <button className="ghost" id="signIn" onClick={() => setButton(true)}>Sign In</button>
       </div>
       <div className="overlay-panel overlay-right">
         <h1>Hello, Friend!</h1>
         <p>Enter your personal details and start journey with us</p>
-        <button className="ghost" id="signUp">Sign Up</button>
+        <button className="ghost" id="signUp" onClick={() =>setButton(false)}>Sign Up</button>
       </div>
     </div>
   </div>
