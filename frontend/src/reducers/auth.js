9c668815cb56b741cebbb75f8738854c225de684
mickeybin2211 <94766118+MagicDinosaur@users.noreply.github.com>
2022-05-20 13:29:23 -0400

    commit

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,9 +1,7 @@
 import {LOGIN_FAIL , LOGIN_SUCCESS , LOGOUT , REGISTER_FAIL , REGISTER_SUCCESS ,} from "../actions/action-types";
 
 const user = JSON.parse ( localStorage.getItem ( "user" ) );
-const initialState = user
-    ? { isLoggedIn : true , user }
-    : { isLoggedIn : false , user : null };
+const initialState = user ? { isLoggedIn : true , user } : { isLoggedIn : false , user : null };
 export default function ( state = initialState , action ) {
     const { type , payload } = action;
     switch ( type ) {
