1679f305e2de504f5e321d4c5ceb8205d3b017e7
mickeybin2211 <94766118+MagicDinosaur@users.noreply.github.com>
2022-05-20 18:20:05 -0400

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
 import Form from './components/User/Form';
-import { Provider } from 'react-redux';
+
 
 function App() {
     return (
