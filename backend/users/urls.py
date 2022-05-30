39507a7c06debe9226b948d93f574904c53e2a8c
apple <phamvduc2112@gmail.com>
2022-05-29 21:36:35 -0400

    create/get message view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('user/register/', views.RegisterView),
     path('user/login/', views.LoginView),
     path('user/logout/', views.LogoutView),
-
     path('chats/', views.ChatView.as_view()),
     path('chats/<uri>/', views.ChatView.as_view()),
     path('chats/<uri>/messages/', views.ChatMessageView.as_view()),
